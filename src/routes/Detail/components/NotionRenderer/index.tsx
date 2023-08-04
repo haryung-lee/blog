2ad9d0bf5d0bdba3c0a1c33a6c030903bb7901d3
leeharyung <lhr4884@naver.com>
2023-08-04 16:56:08 +0900

    fix: code 로딩 추가

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -13,51 +13,56 @@ import "prismjs/themes/prism-tomorrow.css"
 // used for rendering equations (optional)
 
 import "katex/dist/katex.min.css"
-import { FC } from "react"
+import { FC, Suspense } from "react"
 import styled from "@emotion/styled"
 
-const _NotionRenderer = dynamic(
-  () => import("react-notion-x").then((m) => m.NotionRenderer),
-  { ssr: false }
+const _NotionRenderer = dynamic(() =>
+  import("react-notion-x").then((m) => m.NotionRenderer)
 )
 
-const Code = dynamic(() =>
-  import("react-notion-x/build/third-party/code").then(async (m) => {
-    await Promise.all([
-      import("prismjs/components/prism-markup-templating.js"),
-      import("prismjs/components/prism-markup.js"),
-      import("prismjs/components/prism-bash.js"),
-      import("prismjs/components/prism-c.js"),
-      import("prismjs/components/prism-cpp.js"),
-      import("prismjs/components/prism-csharp.js"),
-      import("prismjs/components/prism-docker.js"),
-      import("prismjs/components/prism-java.js"),
-      import("prismjs/components/prism-js-templates.js"),
-      import("prismjs/components/prism-coffeescript.js"),
-      import("prismjs/components/prism-diff.js"),
-      import("prismjs/components/prism-git.js"),
-      import("prismjs/components/prism-go.js"),
-      import("prismjs/components/prism-graphql.js"),
-      import("prismjs/components/prism-handlebars.js"),
-      import("prismjs/components/prism-less.js"),
-      import("prismjs/components/prism-makefile.js"),
-      import("prismjs/components/prism-markdown.js"),
-      import("prismjs/components/prism-objectivec.js"),
-      import("prismjs/components/prism-ocaml.js"),
-      import("prismjs/components/prism-python.js"),
-      import("prismjs/components/prism-reason.js"),
-      import("prismjs/components/prism-rust.js"),
-      import("prismjs/components/prism-sass.js"),
-      import("prismjs/components/prism-scss.js"),
-      import("prismjs/components/prism-solidity.js"),
-      import("prismjs/components/prism-sql.js"),
-      import("prismjs/components/prism-stylus.js"),
-      import("prismjs/components/prism-swift.js"),
-      import("prismjs/components/prism-wasm.js"),
-      import("prismjs/components/prism-yaml.js"),
-    ])
-    return m.Code
-  })
+const Code = dynamic(
+  () =>
+    import("react-notion-x/build/third-party/code").then(async (m) => {
+      await Promise.all([
+        import("prismjs/components/prism-markup-templating.js"),
+        import("prismjs/components/prism-markup.js"),
+        import("prismjs/components/prism-bash.js"),
+        import("prismjs/components/prism-c.js"),
+        import("prismjs/components/prism-cpp.js"),
+        import("prismjs/components/prism-csharp.js"),
+        import("prismjs/components/prism-docker.js"),
+        import("prismjs/components/prism-java.js"),
+        import("prismjs/components/prism-js-templates.js"),
+        import("prismjs/components/prism-coffeescript.js"),
+        import("prismjs/components/prism-diff.js"),
+        import("prismjs/components/prism-git.js"),
+        import("prismjs/components/prism-go.js"),
+        import("prismjs/components/prism-graphql.js"),
+        import("prismjs/components/prism-handlebars.js"),
+        import("prismjs/components/prism-less.js"),
+        import("prismjs/components/prism-makefile.js"),
+        import("prismjs/components/prism-markdown.js"),
+        import("prismjs/components/prism-objectivec.js"),
+        import("prismjs/components/prism-ocaml.js"),
+        import("prismjs/components/prism-python.js"),
+        import("prismjs/components/prism-reason.js"),
+        import("prismjs/components/prism-rust.js"),
+        import("prismjs/components/prism-sass.js"),
+        import("prismjs/components/prism-scss.js"),
+        import("prismjs/components/prism-solidity.js"),
+        import("prismjs/components/prism-sql.js"),
+        import("prismjs/components/prism-stylus.js"),
+        import("prismjs/components/prism-swift.js"),
+        import("prismjs/components/prism-wasm.js"),
+        import("prismjs/components/prism-yaml.js"),
+      ]).catch((err) => {
+        console.error("[Code Dynamic Import Error] ", err)
+      })
+      return m.Code
+    }),
+  {
+    loading: () => <p>Loading...</p>,
+  }
 )
 
 const Collection = dynamic(() =>
@@ -93,20 +98,22 @@ const NotionRenderer: FC<Props> = ({ recordMap }) => {
   const [scheme] = useScheme()
   return (
     <StyledWrapper>
-      <_NotionRenderer
-        darkMode={scheme === "dark"}
-        recordMap={recordMap}
-        components={{
-          Code,
-          Collection,
-          Equation,
-          Modal,
-          Pdf,
-          nextImage: Image,
-          nextLink: Link,
-        }}
-        mapPageUrl={mapPageUrl}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <_NotionRenderer
+          darkMode={scheme === "dark"}
+          recordMap={recordMap}
+          components={{
+            Code,
+            Collection,
+            Equation,
+            Modal,
+            Pdf,
+            nextImage: Image,
+            nextLink: Link,
+          }}
+          mapPageUrl={mapPageUrl}
+        />
+      </Suspense>
     </StyledWrapper>
   )
 }
